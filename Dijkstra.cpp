1f846fc1dd86e760eff62b8c6770ea0c99618878
GreenSnabl <rubensbarr@web.de>
2018-05-11 22:01:44 +0000

    cleaned up

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -34,12 +34,7 @@ struct Position {
     int y;
 
     bool operator!=(const Position &other) const {
-<<<<<<< HEAD
         return (this->x != other.x && this->y != other.y);
-=======
-        if (this->x != other.x || this->y != other.y) return true;
-        return false;
->>>>>>> 3fa900a3840e28a20f690b804c2e7dae371d2721
     }
     bool operator==(const Position &other) const {
         return (this->x == other.x && this->y == other.y);
@@ -78,38 +73,20 @@ struct Edge {
     }
 };
 
-<<<<<<< HEAD
-=======
-struct Mengen {
-        
-    static const int m_max_w = 5;
-    static const int m_max_h = 10;
-    
-    set<Edge> edges;
-    set<Position> vertices;
 
-
->>>>>>> 3fa900a3840e28a20f690b804c2e7dae371d2721
 
     void addVertices(bool arr[4][3], set<Position>& vertices) {
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 3; ++j) {
 
-<<<<<<< HEAD
+
                 if (arr[i][j] == true) vertices.insert({j, i});
-=======
-    void addVertices(bool arr[m_max_h][m_max_w])
-    {
-        for (int i = 0; i < m_max_h; ++i) {
-            for (int j = 0; j < m_max_w; ++j) {
-                
-                if (arr[i][j] == true) vertices.insert({j,i});
->>>>>>> 3fa900a3840e28a20f690b804c2e7dae371d2721
+
             }
         }
     }
 
-<<<<<<< HEAD
+
     void addEdges(bool arr[4][3], set<Edge>& edges) {
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 3; ++j) {
@@ -121,21 +98,7 @@ struct Mengen {
                                     {j, i},
                                     {l, k}
                                 });
-=======
-    void addEdges(bool arr[m_max_h][m_max_w]) 
-    {
-        for (int i = 0; i < 10; ++i) {
-            for (int j = 0; j < 5; ++j)
-            {
-                if (arr[i][j] == true) 
-                {
-                    for (int k = i - 1; k < i + 2; ++k) 
-                    {
-                        for (int l = j - 1; l < j + 2; ++l) 
-                        {
-                            if (k < 0 || k >= 10 || l < 0 || l >= 5 || (k == i && l == j)) continue;
-                            if (arr[k][l] == true) edges.insert({{j,i},{l,k}});                        
->>>>>>> 3fa900a3840e28a20f690b804c2e7dae371d2721
+        
                         }
                     }
                 }
@@ -160,7 +123,6 @@ struct CompareSecond {
     }
 };
 
-<<<<<<< HEAD
 Position minDistance(map<Position, int>& dist) {
     std::pair<Position, int> min = *std::min_element(dist.begin(), dist.end(), CompareSecond());
     return min.first;
@@ -228,11 +190,10 @@ void printGraph(const map<Position, map<Position, int> >& graph) {
     for (auto it : graph)
         for (auto it2 : it.second)
             cout << it.first << "<->" << it2.first << " Distance: " << it2.second << endl;
-    ;
+    
 }
 
 int main(int argc, char** argv) {
-
 
 
     bool arr[4][3]{
@@ -240,26 +201,6 @@ int main(int argc, char** argv) {
         {true, false, false},
         {true, false, true},
         {false, true, true}
- 
-=======
-
-    const int m_max_w = 5;
-    const int m_max_h = 10;
-
-int main(int argc, char** argv) {
-
-    bool arr[10][5] {
-        {false, false, false, false, true},
-        {false, false, true, false, true},
-        {true, false, false, true, false},
-        {false, true, false, true, false},
-        {false, false, true, false, true},
-        {false, false, true, true, false},
-        {false, true, false, false, false},
-        {false, true, true, false, true},
-        {true, false, false, true, false},
-        {true, false, false, true, false}
->>>>>>> 3fa900a3840e28a20f690b804c2e7dae371d2721
     };
 
     map<Position, map<Position, int> > graph;
